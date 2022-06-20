120bc7141f8c111066c26ce827cff8557c33de8a
RabeloGuedes <krepscredian@gmail.com>
2022-06-20 17:15:03 -0300

    Inicia as configurações dos serviços de requisação de API, constroi o arquivo  da página de Login e os componentes Header e SearchBar.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={ Login } />
+      </Switch>
+    </Router>
   );
 }
 
