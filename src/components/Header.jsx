ac3649c73a32f23d0956d4f42d789488a0c61780
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-06-20 20:24:39 -0300

    req 9 - implementando os principais elementos do header: botaode perfil, titulo da pagina e botao de busca

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Profile from '../images/profileIcon.svg';
+import SearchIcon from '../images/searchIcon.svg';
+
+export default function Header() {
+  // const { location: { pathname }, push } = history;
+
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="profile-top-btn"
+      >
+        <img
+          src={ Profile }
+          alt="Profile"
+        />
+      </button>
+      <p data-testid="page-title">Page title</p>
+
+      <button
+        data-testid="search-top-btn"
+        type="button"
+        onClick={ () => setHideShowBar(!hideShowBar) }
+      >
+        <img
+          src={ SearchIcon }
+          alt="Profile"
+        />
+      </button>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
