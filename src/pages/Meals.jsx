efe582d67fd55c0aceec0846d943dcae0d15c7cd
RabeloGuedes <krepscredian@gmail.com>
2022-06-21 14:05:14 -0300

    Repara erros de lint a fim de ser avaliado corretamente no GitHub.

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Meals() {
   return (
-    <div>Meals</div>
+    <div>
+      <Header />
+    </div>
   );
 }
