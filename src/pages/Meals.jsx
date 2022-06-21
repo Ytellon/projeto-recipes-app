f27de15cdde10df2ead13f2830e26e1f0081cab7
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-06-21 15:57:26 -0300

    req 20 - menu inferior fixo e adicionando icones para cada botao

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import BottomMenu from '../components/BottomMenu';
 import Header from '../components/Header';
 
 export default function Meals() {
   return (
     <div>
       <Header title="Foods" />
+      <BottomMenu />
     </div>
   );
 }
