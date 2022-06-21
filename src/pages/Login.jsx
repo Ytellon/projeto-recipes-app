90e12a8df53b7da6e3337ba95bf26bf3e8fa54f5
RabeloGuedes <krepscredian@gmail.com>
2022-06-20 21:16:01 -0300

    Inicia desenvolvimento da página de Login, acrenscentando os campos de email e senha, a função de validação deles e o botão de Login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState();
+
+  useEffect(() => {
+    const habilitarBotao = () => {
+      const caracter = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+      const minimoCarac = 7;
+      if (!(caracter.test(email)) || password.length < minimoCarac) {
+        setIsButtonDisabled(true);
+      } else {
+        setIsButtonDisabled(false);
+      }
+    };
+    habilitarBotao();
+  }, [email, password]);
+
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        data-testid="email-input"
+        value={ email }
+        onChange={ ({ target: { value } }) => setEmail(value) }
+      />
+      <input
+        type="password"
+        placeholder="password"
+        data-testid="password-input"
+        value={ password }
+        onChange={ ({ target: { value } }) => setPassword(value) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isButtonDisabled }
+      >
+        Enter
+      </button>
+    </div>
   );
 }
