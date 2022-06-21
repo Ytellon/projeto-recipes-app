474eefb64686054ce91cda646d970672ab43a407
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-06-21 15:46:13 -0300

    req 19 - implementado elementos do menu inferior: icone de bebida, exploracao e comida

---
+++ src/components/BottomMenu.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+export default function BottomMenu() {
+  const history = useHistory();
+
+  return (
+    <footer data-testid="footer">
+      <button
+        type="button"
+        data-testid="drinks-bottom-btn"
+        onClick={ () => history.push('/drinks') }
+      >
+        <img src={ drinkIcon } alt="Ícone de bebida" />
+      </button>
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+        onClick={ () => history.push('/explore') }
+      >
+        <img src={ exploreIcon } alt="Ícone de exploração" />
+      </button>
+      <button
+        type="button"
+        data-testid="food-bottom-btn"
+        onClick={ () => history.push('/foods') }
+      >
+        <img src={ mealIcon } alt="Ícone de comida" />
+      </button>
+    </footer>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Profile from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
+import BottomMenu from './BottomMenu';
 
 export default function Header(props) {
   const history = useHistory();
@@ -39,6 +40,8 @@ export default function Header(props) {
         data-testid="search-input"
         hidden={ hideShowBar }
       />
+
+      <BottomMenu />
     </div>
   );
 }
