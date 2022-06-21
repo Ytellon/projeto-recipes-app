936b0fbc05cfa82a2a099ce3d9f9db93bf8bc5ec
RabeloGuedes <krepscredian@gmail.com>
2022-06-20 21:41:56 -0300

    Salva os token meals e cocktails no localStorage e redireciona para a rota '/foods', além de cria a página Meals e adiciona o seu respectivo path no App.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Login() {
+export default function Login(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isButtonDisabled, setIsButtonDisabled] = useState();
@@ -14,6 +15,14 @@ export default function Login() {
     };
     habilitarBotao();
   }, [email, password]);
+
+  const redirectToRecipes = () => {
+    const { history } = props;
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/foods');
+  };
 
   return (
     <div>
@@ -35,9 +44,14 @@ export default function Login() {
         type="button"
         data-testid="login-submit-btn"
         disabled={ isButtonDisabled }
+        onClick={ redirectToRecipes }
       >
         Enter
       </button>
     </div>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }),
+}.isRequired;
