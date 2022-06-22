2d87af90a870c542754f832955ce1dc11c73faf8
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-06-22 00:06:13 -0300

    ajustes

--- src/FoodContext/foodContextProvider.js
+++ src/FoodContext/foodContextProvider.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FoodContext } from './foodContext';
+import React from 'react';
+import PropTypes from 'prop-types';
 import FoodContext from './foodContext';
 
 const FoodContextProvider = ({ children }) => {
