6ddfc40837d53a4aa4d168614bfbf9480f827e15
RabeloGuedes <krepscredian@gmail.com>
2022-06-22 22:39:44 -0300

    Muda problemas com o teste do bottom-menu.test.js, remove método '.only' do test.

--- src/tests/bottom-menu.test.js
+++ src/tests/bottom-menu.test.js
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import userEvent from '@testing-library/user-event';
-import Meals from '../pages/Meals';
+import Food from '../pages/Food';
 
 describe.only('Verifica componente Bottom Menu', () => {
   const history = createMemoryHistory();
 
   test('Se existe os ícones de bebida, pesquisa e comida', () => {
-    render(<Meals />);
+    render(<Food />);
 
     const iconDrink = screen.getByTestId('drinks-bottom-btn');
     expect(iconDrink).toBeInTheDocument();
@@ -21,10 +21,10 @@ describe.only('Verifica componente Bottom Menu', () => {
     expect(foodIcon).toBeInTheDocument();
   });
 
-  test.only('Ao clicar nos ícones, é renderizado a suas devidas rotas', () => {
+  test('Ao clicar nos ícones, é renderizado a suas devidas rotas', () => {
     render(
       <MemoryRouter>
-        <Meals />
+        <Food />
       </MemoryRouter>,
     );
 
