c38ca41727edfc926fed2806ca24bfd4745a77fa
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-06-24 17:28:40 -0300

    req 58 - implementando botoes de filtros de comida, bebida e all

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import ShareIcon from '../images/shareIcon.svg';
 
 export default function DoneRecipes() {
-  const drinkOrMeal = 'meal';
+  const [doneRecipes] = useState(JSON
+    .parse(localStorage.getItem('doneRecipes')));
+
+  const [filteredRecipes, setFilteredRecipes] = useState(doneRecipes);
+
+  const filterRecipes = (type) => {
+    if (type === 'meal') {
+      const meals = doneRecipes.filter(({ alcoholicOrNot }) => alcoholicOrNot === '');
+      setFilteredRecipes(meals);
+    } else {
+      const drinks = doneRecipes.filter(({ alcoholicOrNot }) => alcoholicOrNot !== '');
+      setFilteredRecipes(drinks);
+    }
+  };
+
+  const renderAllRecipes = () => {
+    setFilteredRecipes(doneRecipes);
+  };
 
   return (
     <div>
@@ -11,42 +28,59 @@ export default function DoneRecipes() {
         title="Done Recipes"
         showSearchIcon={ false }
       />
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-food-btn">Food</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => renderAllRecipes() }
+      >
+        All
 
-      { drinkOrMeal === 'meal' && (
-        <>
-          <img src="" alt="" data-testid="index-horizontal-image" />
-          <p data-testid="${index-horizontal-top-text">Nacionalidade e Categoria</p>
-          <p data-testid="${index-horizontal-name">Nome da receita</p>
-          <p data-testid="${index-horizontal-done-date">Data</p>
-          <p data-testid="${index-${tagName-horizontal-tag">Tags</p>
-          <button
-            type="button"
-            data-testid="${index-horizontal-share-btn"
-          >
-            <img src={ ShareIcon } alt="Ícone de compartilhar" />
-          </button>
-        </>
-      ) }
-      {
-        drinkOrMeal === 'drink' && (
-          <>
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => filterRecipes('meal') }
+      >
+        Food
+
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => filterRecipes('drinks') }
+      >
+        Drinks
+
+      </button>
+
+      { filteredRecipes
+        .map(({ nationality, alcoholicOrNot, category, name, doneDate }, index) => (
+          <div key={ index }>
             <img src="" alt="" data-testid="index-horizontal-image" />
-            <p data-testid="${index-horizontal-name">Nome da receita</p>
-            <p data-testid="${index-horizontal-top-text">É alcoolico ou não</p>
-            <p data-testid="${index-horizontal-done-date">Data</p>
+            <p
+              data-testid="${index-horizontal-top-text"
+            >
+              { nationality === '' ? alcoholicOrNot : `${nationality} - ${category}` }
+
+            </p>
+            <p data-testid="${index-horizontal-name">{ name }</p>
+            <p data-testid="${index-horizontal-done-date">{ doneDate }</p>
+            <p
+              data-testid="${index-${tagName-horizontal-tag"
+            >
+              { nationality !== '' ? 'renderiza tags' : 'não renderiza' }
+
+            </p>
+
             <button
               type="button"
               data-testid="${index-horizontal-share-btn"
             >
               <img src={ ShareIcon } alt="Ícone de compartilhar" />
-
             </button>
-          </>
-        )
-      }
+          </div>
+        )) }
+
     </div>
   );
 }
