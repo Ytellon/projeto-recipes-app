6a94e721990bbcf820d79fbd94412369327cf260
Ytellon Macedo <ytellonmacedo@gmail.com>
2022-06-26 21:14:22 -0300

    feat: adicionado botoes no explore foods

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 export default function Explore() {
   return (
@@ -7,6 +8,16 @@ export default function Explore() {
       <Header
         title="Explore"
         showSearchIcon={ false }
+      />
+      <Button
+        dataTestIdButton="explore-button-1"
+        name="Explore 1"
+        onClick={ () => console.log('Explore 1') }
+      />
+      <Button
+        dataTestIdButton="explore-button-2"
+        name="Explore 2"
+        onClick={ () => console.log('Explore 2') }
       />
     </div>
   );
