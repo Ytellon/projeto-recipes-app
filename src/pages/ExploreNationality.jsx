800a394650e521593af59f497ab22f52c1bcedd8
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-07-02 17:33:56 -0300

    req 78 - criando dropdown com  todas as nacionalidades das comidas

--- src/pages/ExploreNationality.jsx
+++ src/pages/ExploreNationality.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
+import { getAllMealsInitial, getAllMealsNationalities } from '../service/mealAPI';
 
 export default function ExploreNationality() {
+  const [nationalities, setNationalities] = useState([]);
+  const [meals, setMeals] = useState([]);
+  console.log(meals);
+  useEffect(() => {
+    const areaMeals = async () => {
+      const areas = await getAllMealsNationalities();
+      setNationalities(areas);
+    };
+    areaMeals();
+
+    const getMeals = async () => {
+      const foods = await getAllMealsInitial();
+      setMeals(foods);
+    };
+    getMeals();
+  }, []);
+
   return (
     <div>
       <Header
         title="Explore Nationalities"
         showSearchIcon
       />
+      <select data-testid="explore-by-nationality-dropdown">
+        { nationalities.map(({ strArea }) => (
+          <option
+            key={ strArea }
+            data-testid={ `${strArea}-option` }
+          >
+            { strArea }
+          </option>
+        )) }
+      </select>
+      { meals.map(({ strMeal, strMealThumb }, index) => {
+        const limit = 12;
+        if (index < limit) {
+          return (
+            <div key={ strMeal } data-testid={ `${index}-recipe-card` }>
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ strMealThumb }
+                alt={ strMeal }
+              />
+              <p data-testid={ `${index}-card-name` }>{ strMeal }</p>
+            </div>
+          );
+        }
+        return '';
+      }) }
       <BottomMenu />
     </div>
   );
--- src/service/mealAPI.js
+++ src/service/mealAPI.js
@@ -86,6 +86,28 @@ export const getMealByCategory = async (category) => {
   }
 };
 
+export const getAllMealsNationalities = async () => {
+  const URL = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMealsByNationality = async (nationality) => {
+  const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${nationality}`;
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const getMealRandom = async () => {
   const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
   try {
