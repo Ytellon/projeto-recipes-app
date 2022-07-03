2ad149db02506f4758c83bd955f1c303f53db8be
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-07-03 00:23:40 -0300

    req 79 e 80 - implementando filtros para renderizar comidas por nacionalidade e a opcao All que mostra todas

--- src/pages/ExploreNationality.jsx
+++ src/pages/ExploreNationality.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
-import { getAllMealsInitial, getAllMealsNationalities } from '../service/mealAPI';
+import { getAllMealsInitial,
+  getAllMealsNationalities, getMealsByNationality } from '../service/mealAPI';
 
 export default function ExploreNationality() {
   const [nationalities, setNationalities] = useState([]);
+
   const [meals, setMeals] = useState([]);
-  console.log(meals);
+  const [filteredFoods, setFilteredFoods] = useState([]);
+
+  const history = useHistory();
+
   useEffect(() => {
     const areaMeals = async () => {
       const areas = await getAllMealsNationalities();
@@ -17,9 +23,18 @@ export default function ExploreNationality() {
     const getMeals = async () => {
       const foods = await getAllMealsInitial();
       setMeals(foods);
+      setFilteredFoods(foods);
     };
     getMeals();
   }, []);
+
+  const filterMealsByNationality = async ({ target: { value } }) => {
+    if (value === 'All') {
+      return setFilteredFoods(meals);
+    }
+    const foods = await getMealsByNationality(value);
+    return setFilteredFoods(foods);
+  };
 
   return (
     <div>
@@ -27,27 +42,40 @@ export default function ExploreNationality() {
         title="Explore Nationalities"
         showSearchIcon
       />
-      <select data-testid="explore-by-nationality-dropdown">
+      <select
+        data-testid="explore-by-nationality-dropdown"
+        onChange={ (event) => filterMealsByNationality(event) }
+      >
+        <option data-testid="All-option">All</option>
         { nationalities.map(({ strArea }) => (
           <option
-            key={ strArea }
             data-testid={ `${strArea}-option` }
+            key={ strArea }
+            value={ strArea }
           >
             { strArea }
           </option>
         )) }
       </select>
-      { meals.map(({ strMeal, strMealThumb }, index) => {
+      { filteredFoods.map(({ strMeal, strMealThumb, idMeal }, index) => {
         const limit = 12;
         if (index < limit) {
           return (
-            <div key={ strMeal } data-testid={ `${index}-recipe-card` }>
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ strMealThumb }
-                alt={ strMeal }
-              />
-              <p data-testid={ `${index}-card-name` }>{ strMeal }</p>
+            <div
+              data-testid={ `${index}-recipe-card` }
+              key={ strMeal }
+            >
+              <button
+                type="button"
+                onClick={ () => history.push(`/foods/${idMeal}`) }
+              >
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ strMealThumb }
+                  alt={ strMeal }
+                />
+                <p data-testid={ `${index}-card-name` }>{ strMeal }</p>
+              </button>
             </div>
           );
         }
